a61c8eb783964c32bb5bdb19ec80997879fde8bd
pttiming <sean@pttiming.com>
2020-10-04 19:22:32 -0500

    Add Method Tests Added

--- CustomListTest/AddTest.cs
+++ CustomListTest/AddTest.cs
@@ -70,7 +70,7 @@ namespace CustomListTest
             CustomList<List<String>> customList = new CustomList<List<string>>();
             List<String> strings1 = new List<string>() { "Bob", "Chris", "Mike" };
             List<String> strings2 = new List<string>() { "Smith", "Wallace", "Jones" };
-            
+
 
             int expected = 2;
             int actual;
@@ -123,7 +123,40 @@ namespace CustomListTest
 
         }
 
+        [TestMethod]
+        public void Add_TenPositiveValue_Capacity16()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int value6 = 6;
+            int value7 = 7;
+            int value8 = 8;
+            int value9 = 9;
+            int value10 = 10;
 
+            int expected = 16;
+            int actual;
+
+            //Act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+            customList.Add(value5);
+            customList.Add(value6);
+            customList.Add(value7);
+            customList.Add(value8);
+            customList.Add(value9);
+            customList.Add(value10);
+            actual = customList.Capacity;
+
+
+        }
     }
 }
 
---
+++ CustomListTest/SubtractionOverloadTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class SubtractionOverloadTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
